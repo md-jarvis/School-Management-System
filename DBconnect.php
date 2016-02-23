6fa8ec86b07a985d69a4397feeb271f54ab1141a
md-jarvis <sadik.nitp18@gmail.com>
2016-02-23 20:10:57 +0530

    define related variable for DB connection

--- DBconnect.php
+++ DBconnect.php
@@ -3,7 +3,7 @@
 first make a ppage to define all the constants
  than make a page to implement classes
  and then implement it in your html classes
-
+ definig all variable to use data base
 */
 define("HOST","localhost");
 define("USER","root");
